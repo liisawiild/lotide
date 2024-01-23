ddbae00b8105bff995f4cee22e3f63a367be457b
Lisa Wild <lisakwild@gmail.com>
2024-01-22 20:20:34 -0800

    reviewed for flex program

--- tail.js
+++ tail.js
@@ -6,15 +6,24 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-
-
-
 const tail = function(array) {
   let newArray = array.slice(1);
+  console.log({array, newArray}); // this returns the original and resulting array
   return newArray;
 };
 
+let abcArray = ["a", "b", "c"];
+let emptyArray = [];
+let oneArray = [2];
 
-const result = tail(["a", "b", "c"]);
+const result = tail(abcArray);
 assertEqual(result.length, 2);
 assertEqual(result[0], "b");
+
+const emptyResult = tail(emptyArray);
+assertEqual(emptyResult.length, 0);
+assertEqual(emptyResult[0], " ");
+
+const oneResult = tail(oneArray);
+assertEqual(oneResult.length, 0);
+assertEqual(oneResult[0], " ");
