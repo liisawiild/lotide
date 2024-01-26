16330019c220d466ac4290b1f05b253c791dd43e
Lisa Wild <lisakwild@gmail.com>
2024-01-25 20:08:32 -0800

    created comparing 2 arrays function

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+let arrayOne = [1, 2, 3];
+let arrayTwo = [1, 2, 3];
+
+const eqArrays = function(arr1, arr2) {
+  //loop through arr1
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
+      return false;
+    }
+  } return true;
+}
+console.log(eqArrays(arrayOne, arrayTwo));
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
