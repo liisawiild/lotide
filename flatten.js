ec773f935335696b5ce0b84b8e1805cbed97b036
Lisa Wild <lisakwild@gmail.com>
2024-01-25 23:59:35 -0800

    first commit

---
+++ flatten.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    } return true;
+  }
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+//new function
+
+const flatten = function(array) {
+let flatArray = [];
+for (let i = 0; i < array.length; i++) {
+  if (Array.isArray(array[i])) {
+    for (let ii = 0; ii < array[i].length; ii++) {
+    flatArray.push(array[i][ii]);
+    }
+   } else {
+      flatArray.push(array[i]);
+  }
+} return flatArray;
+};
+
+//TEST CODE
+const myArray = [1, 2, [3, 4], 5, [6, 7], 6];
+console.log(flatten(myArray));
