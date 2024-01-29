8e1cf86d1faf18d6178e80e91e4577ef0d774ed1
Lisa Wild <lisakwild@gmail.com>
2024-01-29 13:15:57 -0800

    added loop through object

--- countOnly.js
+++ countOnly.js
@@ -10,6 +10,10 @@ const assertEqual = function(actual, expected) {
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
+   
+  for (const item of allItems) {
+    console.log(item);
+  }
   return results;
 };
 
