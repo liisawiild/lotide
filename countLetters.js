0c4d9d7ff839d45fc6939368ecd725faad5397ea
Lisa Wild <lisakwild@gmail.com>
2024-01-29 13:36:01 -0800

    added in loop through word

--- countLetters.js
+++ countLetters.js
@@ -7,10 +7,16 @@ const assertEqual = function(actual, expected) {
 };
 
 
-const countLetters = function() {
+const countLetters = function(word) {
   let letterCount = {};
+
+  for (var letter of word) {
+    console.log(letter);
+  }
 //take a string
 // loop through the letters in the string
 // count how many of each letter present
   return letterCount;
 };
+
+countLetters("Lisa Wild");
