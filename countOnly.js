087d2693da2fa3ac7cbbefe3dadab66310f632e4
Lisa Wild <lisakwild@gmail.com>
2024-01-29 13:14:52 -0800

    added new object and it's return

--- countOnly.js
+++ countOnly.js
@@ -9,7 +9,8 @@ const assertEqual = function(actual, expected) {
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
-
+  const results = {};
+  return results;
 };
 
 // TEST CODE
