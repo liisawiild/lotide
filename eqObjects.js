5427fa4cf0a32cf974c2f1d0a0acc996cc17d1a6
Lisa Wild <lisakwild@gmail.com>
2024-01-31 09:32:14 -0800

    eqObjects definition and pseudocode

---
+++ eqObjects.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function(obj1, obj2) {
+// returns true if obj1 and obj2 match
+// returns false if obj1 and obj2 do not match
+}; 
