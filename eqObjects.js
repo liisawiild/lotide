d88bb0856f5904d15a2e69cbfc4593274ff7fad4
Lisa Wild <lisakwild@gmail.com>
2024-01-31 09:38:15 -0800

    added test code and assertEqual checks

--- eqObjects.js
+++ eqObjects.js
@@ -10,3 +10,14 @@ const eqObjects = function(obj1, obj2) {
 // returns true if obj1 and obj2 match
 // returns false if obj1 and obj2 do not match
 }; 
+
+// TEST CODE
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+eqObjects(shirtObject , anotherShirtObject); // => true
+assertEqual((eqObjects(shirtObject , anotherShirtObject)), true);
+
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject , longSleeveShirtObject); // => false
+assertEqual((eqObjects(shirtObject , longSleeveShirtObject)), false);
