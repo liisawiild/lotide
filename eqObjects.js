08f93a261423b10ce9cb64825b7b9042cf40f16c
Lisa Wild <lisakwild@gmail.com>
2024-01-31 11:03:40 -0800

    cleaned up code for submission

--- eqObjects.js
+++ eqObjects.js
@@ -7,27 +7,18 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqObjects = function(obj1, obj2) {
-// returns true if obj1 and obj2 match = same number of keys, key:values match in each object
   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
     return false;
   } 
-  console.log(obj1);
-  console.log(obj2, "Object 2");
-  console.log(Object.keys(obj1));
-   for (const key of Object.keys(obj1)) {
-    console.log(key, "beginning of loop");
-    console.log(obj1[key], "object 1");
-    console.log(obj2[key], "Object 2");
-    if (obj1[Object.keys[key]] !== Object.keys(obj2)[key]) {
-      return false;
-    }
+    for (const key of Object.keys(obj1)) {
+      if (obj1[key] !== obj2[key]) {
+        return false;
+      }
    } return true;
-// returns false if obj1 and obj2 do not match = diff number of keys or key:value pairs don't match
-
 }; 
 
-// TEST CODE
 
+// TEST CODE
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject= { size: "medium", color: "red" };
 eqObjects(shirtObject , anotherShirtObject); // => true
