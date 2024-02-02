8278b424482030e989e7fae1229f33527cfc9013
Lisa Wild <lisakwild@gmail.com>
2024-02-02 14:04:31 -0800

    added in an assertion test for an undefined result

--- findKey.js
+++ findKey.js
@@ -22,3 +22,4 @@ console.log(results);
 
 let myObject = {"Lisa": {dog: 1}, "Lyla": {dog: 2}};
 assertEqual(findKey(myObject, value => value.dog > 1), "Lyla");
+assertEqual(findKey(myObject, value => value.dog > 3), undefined);
