e47092db7e66c927c188de4cadd1f3ad13658c51
Lisa Wild <lisakwild@gmail.com>
2024-02-13 12:14:03 -0800

    removed assertEqual and test code, and used require and module.export to keep tests working

--- tail.js
+++ tail.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const tail = function(array) {
   let newArray = array.slice(1);
@@ -12,21 +5,4 @@ const tail = function(array) {
   return newArray;
 };
 
-let abcArray = ["a", "b", "c"];
-let emptyArray = [];
-let oneArray = [2];
-
-const result = tail(abcArray);
-assertEqual(abcArray.length, 3);
-assertEqual(result.length, 2);
-assertEqual(result[0], "b");
-
-const emptyResult = tail(emptyArray);
-assertEqual(emptyArray.length, 0);
-assertEqual(emptyResult.length, 0);
-assertEqual(emptyResult[0], undefined);
-
-const oneResult = tail(oneArray);
-assertEqual(oneArray.length, 1);
-assertEqual(oneResult.length, 0);
-assertEqual(oneResult[0], undefined);
+module.exports = tail;
