8076c751fc182c5856f57acc00089455936b9c87
Lisa Wild <lisakwild@gmail.com>
2024-02-13 09:28:47 -0800

    refactored with feedback from Larry

--- eqObjects.js
+++ eqObjects.js
@@ -9,27 +9,26 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    } 
-    return true;
   } 
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } 
+  return true;
 };
 
 const eqObjects = function(obj1, obj2) {
   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
     return false;
   }
-  for (const key of Object.keys(obj1)) { 
-    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) { 
-      if (!eqArrays(obj1[key], obj2[key])) { 
+  for (const key of Object.keys(obj1)) { //the key is the first key in the array = in this example "color"
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) { //if the key's value an array
+      if (!eqArrays(obj1[key], obj2[key])) { //run through our eqArray function to check if object key values (arrays) aren't equal
         return false;
       }
     } else {
-      if (obj1[key] !== obj2[key]) { 
+      if (obj1[key] !== obj2[key]) { //and then it is pulling the key back into the object to find the value
         return false;
       }
     }
