4fb5f10083749c26c318f59e2ad457bbea1aa5b8
Lisa Wild <lisakwild@gmail.com>
2024-02-13 14:03:06 -0800

    removed old test code

--- test/headTest.js
+++ test/headTest.js
@@ -19,16 +19,3 @@ describe("#head", () => {
   });
 
 });
-
-
-
-
-
-
-// const assertEqual = require('../assertEqual');
-// const head = require('../head');
-
-// //TEST CODE
-// assertEqual(head([1,2,3]), 1);
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
