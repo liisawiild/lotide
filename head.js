2a3da957d181fb9f6b314bbdf4a2f09cb7688588
Lisa Wild <lisakwild@gmail.com>
2024-02-13 09:29:51 -0800

    separated test code into a test file/folder; made externally accessible

--- head.js
+++ head.js
@@ -1,22 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-//inputs = an array
-let testArray = ["a", "b", "c"];
+/* in this code we are running an array through a function to return the 
+   first value, and testing that the results you expect are what is produced 
+   by running it through the assertEqual function */
 
-//processing
 const head = function(array) {
   return array[0];
 };
 
-
-//output
-console.log(head(testArray));
-
-assertEqual(head([1,2,3]), 1);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
