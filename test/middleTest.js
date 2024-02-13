e3b60f4e78c92218f9e1a8a2e45796c062f142b0
Lisa Wild <lisakwild@gmail.com>
2024-02-13 13:26:11 -0800

    pulled out assertion functions to a separate test file/folder

---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+//TEST CODE
+let myArray = [1, 2, 3, 4, 5, 8, 9, 7, 5, 6 ,1]
+console.log(middle(myArray));
+
+let array2 = [1, 2, 3, 4];
+let expectedArray = [2, 3];
+console.log(middle(array2));
+
+assertArraysEqual(middle(array2), expectedArray);
