283d765355591a5d35735b6c1522aa42e02e53e8
Lisa Wild <lisakwild@gmail.com>
2024-02-13 09:32:07 -0800

    refactored with feedback from Larry (eqArrays)

--- tail.js
+++ tail.js
@@ -17,13 +17,16 @@ let emptyArray = [];
 let oneArray = [2];
 
 const result = tail(abcArray);
+assertEqual(abcArray.length, 3);
 assertEqual(result.length, 2);
 assertEqual(result[0], "b");
 
 const emptyResult = tail(emptyArray);
+assertEqual(emptyArray.length, 0);
 assertEqual(emptyResult.length, 0);
-assertEqual(emptyResult[0], " ");
+assertEqual(emptyResult[0], undefined);
 
 const oneResult = tail(oneArray);
+assertEqual(oneArray.length, 1);
 assertEqual(oneResult.length, 0);
-assertEqual(oneResult[0], " ");
+assertEqual(oneResult[0], undefined);
